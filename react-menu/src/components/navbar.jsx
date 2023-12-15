dad4b9bb1f2506948bd9fbc8a67fa3d5ae3ccd63
Проходная <nikolayromanchuk88@gmail.com>
2023-12-15 23:53:49 +0300

    15_12_23

--- react-menu/src/components/navbar.jsx
+++ react-menu/src/components/navbar.jsx
@@ -3,7 +3,7 @@
 export default function Navbar(props) {
     return (
       <nav className="navbar">
-        <ul className="navbar-ul">{props.children}</ul>
+        <ul className="navbar-nav">{props.children}</ul>
       </nav>
     );
 }
--- react-menu/src/index.js
+++ react-menu/src/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import Navbar from './components/navbar';
 import NavItem from './components/navitem';
+
+
+import DropdownMenu from './components/dropdown-menu';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar>
-      <NavItem />
-      <NavItem />
-      <NavItem />
-      <NavItem >
-        <DropdownMenu></DropdownMenu>
-      </NavItem>
-    </Navbar>
+    <nav className="navbar">
+      <ul className="navbar-ul">
+        <NavItem icon={<PlusIcon />} />
+        <NavItem icon={<BellIcon />} />
+        <NavItem icon={<MessengerIcon />} />
+        <NavItem icon={<CaretIcon />}>
+          <DropdownMenu></DropdownMenu>
+        </NavItem>
+      </ul>
+    </nav>
   </React.StrictMode>,
 );
+
+
