02b314780a4dc9d8aceae567e794a5bcf199021a
Strnikolay <nikolayromanchuk88@gmail.com>
2024-01-06 23:29:21 +0300

    06_01_24

---
+++ first-screen-react/src/pages/main/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {Header} from "../../components/header/header"
+import MainFirstScreen from "./scr1/screen1";
+import MainSecondScreen from "./scr2/screen2";
+import MainThirdScreen from "./scr3/screen3";
+import Footer from "../../components/footer/footer";
+import { Context } from 'index';
+
+
+
+
+function Main () {
+    /*const store = React.useContext(Context)
+    const [state, setState] = React.useState()
+
+    React.useEffect(() =>{
+        console.log(state)
+    }, [])*/
+
+    return(    
+        <>
+            {/*<Header/>*/}
+            <MainFirstScreen/>
+            {/*<MainSecondScreen/>
+            <MainThirdScreen/>
+    <Footer/>*/}
+        </>
+    )
+}
+
+export default Main; 
---
+++ first-screen-react/src/pages/page2/scr2/screen2.jsx
@@ -0,0 +1,15 @@
+import "./screen2.css"
+
+
+function Page2SecondScreen () {
+
+return(
+<div className="screen2">
+
+</div>
+)}
+export default Page2SecondScreen;
+
+
+
+
